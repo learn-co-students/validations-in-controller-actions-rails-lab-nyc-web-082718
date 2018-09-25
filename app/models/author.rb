9701ad9fa423dca548d40994e0ef5911214fa96a
github username <github email address>
2018-09-25 10:34:24 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
+
+
+    def create
+      @author = Author.new(author_params)
+      if @author.valid?
+        @author.save
+        redirect_to author_path(@author)
+      else
+        render :new
+      end
+    end
 end
